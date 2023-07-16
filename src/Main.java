811ea3c20ad8a8f07531991bde86a87668e64e5d
Romchig <romulusto@gmail.com>
2023-07-16 19:16:15 +0200

    начало билета 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
     public static void changeArray(int[] inputArray) {
         for (int i = 0; i < inputArray.length; i++) {
             if (inputArray[i] % 2 == 0) {
@@ -8,12 +9,13 @@ public class Main {
         for (int j = 2; j < 7; j++) {
             System.out.println(inputArray[j]);
         }
+
     }
 
+
     public static void main(String[] args) {
-
         int[] inputArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
         changeArray(inputArray);
+
     }
 }
